13b2a9c6c06276f2e3697b6be2b088d0c42ee4f4
Bob <atomicsteamend@gmail.com>
2021-03-13 07:15:25 -0800

    Added text output

--- src/app/DisplayManager.cpp
+++ src/app/DisplayManager.cpp
@@ -84,6 +84,7 @@ void DisplayManager::Render(DisplayData displayData) {
                    phase.index() * channelWidth + channelOffset - (moonImages.at(phase.value().segment)->width / 2),
                    0);
     }
+    drawBigString(buffer, "Mystic Rythms", 384, 384, 0xFF00FF);
     CopyBuffer();
 }
 
